02db504679713dc4add4db24e59eb7134c8dc66e
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2021-01-09 13:32:46 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,54 @@
+class Food{
+    constructor(){
+       this.milkimg=loadImage("virtual pet images/milk.png");
+     //   milkimg=this.image;
+     
+
+        this.foodStock=0;
+    }
+
+    display(){
+        var x=80,y=100;
+
+        imageMode(CENTER);
+        image(this.milkimg,500,220,70,70);
+
+        if(this.foodStock!=0){
+            for(var i=0;i<this.foodStock;i=+1){
+                if(i%10==0){
+                    x=80;
+                    y=y+50;
+                }
+                image(this.milkimg,x,y,50,50);
+                x=x+30;
+            }
+        }
+    }
+
+    updateFoodStock(foodStock){
+      this.foodStock=foodStock 
+    }
+
+    getFoodStock(){
+     return this.foodStock;
+    }
+
+    bedroom(){
+
+        background(bedImg,550,500);
+
+    }
+
+    garden(){
+
+        background(gardenImg,550,500);
+
+    }
+
+    washroom(){
+
+        background(washroomImg,550,500);
+
+    }
+
+}
---
+++ sketch.js
@@ -0,0 +1,139 @@
+  //Create variables here
+  var dog,foodS,foodStock, database;
+  var dogImg,dogImg2;
+
+  var milkimg;
+  var lastFed, fedTime, foodObj;
+  var food1, gameState;
+  var readState, bedImg, washroomImg, gardenImg;
+
+  function preload()
+  {
+    //load images here
+    dogImg=loadImage("virtual pet images/Dog.png");
+    dogImg2=loadImage("virtual pet images/Happy.png");
+    bedImg=loadImage("virtual pet images/Bed Room.png");
+    washroomImg=loadImage("virtual pet images/Wash Room.png");
+    gardenImg=loadImage("virtual pet images/Garden.png");
+  }
+
+  function setup() {
+    createCanvas(600, 500);
+
+    dog=createSprite(250,300,20,20)
+    dog.addImage(dogImg);
+    dog.scale=0.2;
+
+    database=firebase.database();
+    foodStock=database.ref("Food");
+    foodStock.on("value",readStock);
+
+    food1=new Food();
+
+    feed=createButton("Feed the Dog");
+    feed.position(700,95);
+    feed.mousePressed(feedDog);
+    
+    addFood=createButton("Add Food");
+    addFood.position(800,95);
+    addFood.mousePressed(addFoods);
+
+    readState=database.ref('gameState');
+    readState.on("value",function(data){
+      gameState=data.val();
+    })
+
+  }
+
+  function draw() {  
+    background(46,139,87);
+
+   /* if ( keyWentDown( UP_ARROW ) ){
+      writeStock(foodS);
+      dog.addImage(dogImg2);
+    } */
+
+    noStroke();
+    fill("white")
+    textSize("90")
+    text("Note:Press UP_ARROW key to feed Drago Milk",80,30);
+
+    fill("blue")
+    textSize("60")
+    text("Food Remaining:"+ foodS,170,200);
+
+    food1.display();
+
+    fedTime=database.ref('FeedTime');
+    fedTime.on("value",function(data){
+      lastFed=data.val();
+    })
+
+    if(lastFed>=12){
+      text("Last Feed :"+lastFed%12 +"PM",350,30);
+    }
+    else
+    if(lastFed==0){
+      text("Last Feed : 12 AM",270,60);
+    }
+    else{
+    text("Last Feed :" + lastFed + "AM",350,30);
+    }
+
+    if(gameState!="Hungry"){
+      feed.hide();
+      addFood.hide();
+      dog.remove();
+    }else{
+      feed.show();
+      addFood.show();
+      dog.addImage(dogImg);
+    }
+
+    currentTime=hour();
+    if(currentTime==(lastFed+1)){
+      update("Playing");
+      food1.garden();
+    }else if(currentTime==(lastFed+2)){
+      update("Sleeping");
+      food1.bedroom();
+    }else if(currentTime>(lastFed+1) && currentTime<=(lastFed+4)){
+      update("Bathing");
+      food1.washroom();
+    }else{
+      update("Hungry");
+      food1.display();
+    }
+
+    drawSprites();
+  }
+
+  function readStock( data ){
+
+    foodS=data.val();
+    food1.updateFoodStock(foodS);
+
+  }
+
+  function feedDog(){
+    dog.addImage(dogImg2);
+
+    food1.updateFoodStock(food1.getFoodStock()-1);
+    database.ref('/').update({
+      Food: food1.getFoodStock(),
+      FeedTime:hour()
+    })
+  }
+
+  function addFoods(){
+    foodS++;
+    database.ref('/').update({
+      Food:foodS
+    })
+  }
+
+  function update(state){
+    database.ref('/').update({
+      gameState:state
+    })
+  }
